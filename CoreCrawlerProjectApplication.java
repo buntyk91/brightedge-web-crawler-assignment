ecdaa1f6dbf40005672a907121cc6b8cc9b40ae7
buntyk91 <buntyk91@gmail.com>
2025-06-22 20:53:04 +0530

    Add files via upload

---
+++ CoreCrawlerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.brightedge.core_crawler_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreCrawlerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreCrawlerProjectApplication.class, args);
+	}
+
+}
